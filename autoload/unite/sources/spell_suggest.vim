jbking/unite-spell-suggest

commit 7f44bdde01411db1ccc73eb512694c3798c1ce4b

Fix 'replace' action on first word in line

autoload/unite/sources/spell_suggest.vim
@@ -104,7 +104,9 @@ function! s:replace_word(word, replacement)
   let l:line = getline(a:word.line)
   let l:col  = match(l:line[:a:word.col], '\<\w\+$')
   if l:col > -1
-    call setline(a:word.line, l:line[:l:col-1] . a:replacement . l:line[l:col+len(a:word.word):])
+    let l:head = l:col > 1 ? l:line[l:col-1] : ''
+    let l:tail = l:line[l:col+len(a:word.word):]
+    call setline(a:word.line, l:head . a:replacement . l:tail)
   endif
 endfunction
 

commit 6582161801e60b7056c69e23ccd53846947214dc

Fix replace clobbering head of line
Regression introduced in 7f44bdde01411db1ccc73eb512694c3798c1ce4b

autoload/unite/sources/spell_suggest.vim
@@ -104,7 +104,7 @@ function! s:replace_word(word, replacement)
   let l:line = getline(a:word.line)
   let l:col  = match(l:line[:a:word.col], '\<\w\+$')
   if l:col > -1
-    let l:head = l:col > 1 ? l:line[l:col-1] : ''
+    let l:head = l:col > 1 ? l:line[:l:col-1] : ''
     let l:tail = l:line[l:col+len(a:word.word):]
     call setline(a:word.line, l:head . a:replacement . l:tail)
   endif